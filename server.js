8c60ed4b8b17339e832b805c5545a5246cc57ad3
GUY-cpu-sketch <masonewyatt1@gmail.com>
2025-10-07 14:11:10 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
-const fetch = require('node-fetch');
 const path = require('path');
 
 const app = express();
